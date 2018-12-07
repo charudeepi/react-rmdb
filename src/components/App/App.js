802d06ce0e9c634790541026adb05a1ab7352116
charudeepi <charudeepi@gmail.com>
2018-12-07 18:28:19 -0500

    deploying app

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -7,7 +7,7 @@ import NotFound from '../elements/NotFound/NotFound';
 
 const App = () => {
     return (
-        <BrowserRouter>
+        <BrowserRouter basename='/react-rmdb/'>
             <React.Fragment>
                 <Header/>
                 <Switch>
